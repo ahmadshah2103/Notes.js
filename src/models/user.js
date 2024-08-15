fe12ab410b4ca301e22da54733316954b6513b0c
Ahmad Shah <ahmad1911491@gmail.com>
2024-08-15 18:43:52 +0500

    Created 'User' model, made migrations and ran migrations

---
+++ src/models/user.js
@@ -0,0 +1,53 @@
+const { DataTypes } = require('sequelize');
+
+module.exports = (sequelize) => {
+    const User = sequelize.define('User', {
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: true
+        },
+        google_sub: {
+            type: DataTypes.STRING,
+            allowNull: true,
+            unique: true
+        },
+        username: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true
+        },
+        dob: {
+            type: DataTypes.DATE,
+            allowNull: false
+        },
+        createdAt: {
+            type: DataTypes.DATE,
+            allowNull: false,
+            defaultValue: DataTypes.NOW
+        },
+        updatedAt: {
+            type: DataTypes.DATE,
+            allowNull: false,
+            defaultValue: DataTypes.NOW
+        }
+    }, {
+        tableName: 'users',
+        underscored: true
+    })
+
+    return User
+}
