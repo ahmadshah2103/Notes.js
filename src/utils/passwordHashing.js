b68222ff570fd7de77fbe3d1b9f003a0ba5fbda1
Ahmad Shah <ahmad1911491@gmail.com>
2024-08-17 12:13:26 +0500

    Renamed hashPassword.js to passwordHashing.js and added a verifyPassword function.

--- src/utils/passwordHashing.js
+++ src/utils/passwordHashing.js
@@ -5,4 +5,8 @@ const hashPassword = async (password) => {
     return await bcrypt.hash(password, saltRounds);
 };
 
-module.exports = hashPassword;
+const verifyPassword = async (password, hash) => {
+    return await bcrypt.compare(password, hash);
+};
+
+module.exports = {hashPassword, verifyPassword};
