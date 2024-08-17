1fce141c9db578595899cea9c98f4f3c7cb9a353
Ahmad Shah <ahmad1911491@gmail.com>
2024-08-17 12:15:29 +0500

    Added controller and route for signin API

--- src/routes/user.js
+++ src/routes/user.js
@@ -2,7 +2,9 @@ const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/userController');
 const validateSignup = require('../middlewares/validateSignup');
+const validateSignin = require('../middlewares/validateSignin');
 
 router.post('', validateSignup, userController.signup);
+router.post('/signin', validateSignin, userController.signin);
 
 module.exports = router;
