0836c330b52065ab0b0dfce2fec2ff10ad24b647
Ahmad Shah <ahmad1911491@gmail.com>
2024-08-22 20:37:10 +0500

    Updated /src/configs/config.js

--- src/configs/config.js
+++ src/configs/config.js
@@ -2,10 +2,10 @@ require('dotenv').config();
 
 module.exports = {
     development: {
-        username: process.env.DB_USER,
-        password: process.env.DB_PASSWORD,
-        database: process.env.DB_NAME,
-        host: process.env.DB_HOST,
+        username: process.env.DB_USER || 'root',
+        password: process.env.DB_PASSWORD || '1admin!+user1',
+        database: process.env.DB_NAME || 'notes',
+        host: process.env.DB_HOST || '127.0.0.1',
         port: 3306,
         dialect: 'mysql',
     },
