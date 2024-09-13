f6604075015e2bbdc9520e51acdbafd6abed67fd
Ahmad Shah <ahmad1911491@gmail.com>
2024-09-13 16:19:15 +0500

    Update CORS configuration for localhost development

--- app.js
+++ app.js
@@ -6,14 +6,20 @@ const handleErrors = require("./src/middlewares/handleErrors");
 const logErrors = require("./src/middlewares/logErrors");
 const initializeDatabase = require("./src/configs/database");
 const bodyParser = require('body-parser');
-const { authRateLimiter, notesRateLimiter } = require('./src/controllers/rateLimiters');
+const { authRateLimiter, notesRateLimiter } = require('./src/middlewares/rateLimiters');
 
 const app = express();
 app.use(bodyParser.json());
-app.use(cors())
+app.use(cors(
+  {
+    origin: 'http://localhost:5173',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  }
+))
 
 initializeDatabase()
-  .then(() => console.log('Database connected!s'))
+  .then(() => console.log('Database connected!'))
   .catch(err => console.error('Failed to connect to Database:', err));
 
 app.use('/api/users', authRateLimiter, userRoutes);
@@ -23,6 +29,6 @@ app.use(logErrors);
 app.use(handleErrors);
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
+app.listen(PORT, 'localhost', () => {
     console.log(`Server is running on port ${PORT}`);
 });
